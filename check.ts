124a44d4c52dda851e2d1a4c71aeddc20bf1bb68
an-ikhtiyour <an.ikhtiyour@gmail.com>
2021-06-24 10:15:46 +0500

    first problems

---
+++ check.ts
@@ -0,0 +1,16 @@
+function check(a:number){
+if(a>0){
+    console.log("it is a positive",a);
+}
+else if(a==0){
+    console.log("it is a zero");
+}
+else if(a<0){
+    console.log("it is a negative number",a);
+}
+else{
+   console.log(a);
+}
+}
+
+check(15);
